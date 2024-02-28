d9f5827ca047a6f7abc26c951ed230701dab0d90
tombazan <tomasbazan95@gmail.com>
2024-02-28 11:56:33 -0300

    Se modifico nuevoTransporte.php para que se pueda hacer cargas masivas de vehiculos y choferes en transporte

--- nuevoTransporte.php
+++ nuevoTransporte.php
@@ -1,14 +1,16 @@
 <?php
     require("config.php");
-    if(empty($_SESSION['user']))
-    {
-        header("Location: index.php");
-        die("Redirecting to index.php"); 
+    if(empty($_SESSION['user']['id_perfil'])){
+      header("Location: index.php");
+      die("Redirecting to index.php"); 
     }
 	
 	require 'database.php';
 
 	if ( !empty($_POST)) {
+    // var_dump($_POST);
+    // die;
+
 		// insert data
 		$pdo = Database::connect();
 		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -16,6 +18,27 @@
 		$sql = "INSERT INTO transportes(razon_social, cuit, domicilio,id_usuario, fecha_hora_alta) VALUES (?,?,?,?,now())";
 		$q = $pdo->prepare($sql);
 		$q->execute(array($_POST['razon_social'], $_POST['cuit'],$_POST['domicilio'],$_SESSION['user']['id_perfil']));
+    $idTransporte = $pdo->lastInsertId();
+
+    foreach($_POST["nombre_apellido"] as $key => $nombre_apellido){
+      if($key==0){
+        continue;
+      }
+      
+      $sql = "INSERT INTO choferes (nombre_apellido, dni, id_transporte, id_usuario) VALUES (?,?,?,?) ";
+      $q = $pdo->prepare($sql);
+      $q->execute(array($nombre_apellido,$_POST["dni"][$key],$idTransporte,$_SESSION['user']['id_perfil']));
+    }
+    
+    foreach($_POST["descripcion"] as $key => $descripcion){
+      if($key==0){
+        continue;
+      }
+
+      $sql = "INSERT INTO vehiculos (descripcion, patente, patente2, id_transporte, id_usuario) VALUES (?,?,?,?,?) ";
+      $q = $pdo->prepare($sql);
+      $q->execute(array($descripcion,$_POST["patente"][$key],$_POST["patente2"][$key],$idTransporte,$_SESSION['user']['id_perfil']));
+    }
 		
 		Database::disconnect();
 		
@@ -71,106 +94,82 @@
               <div class="col-sm-12">
                   <div class="card">
                     <div class="card-header">
-                      <h5 class="text-center">Nuevo Transporte</h5>
+                      <h5>Nuevo Transporte</h5>
                     </div>
                     <form class="form theme-form" role="form" method="post" action="nuevoTransporte.php">
                       <div class="card-body">
                         <div class="row">
                           <div class="col">
-                          <div class="form-group row">
-                            <div class="form-group col-4">
-                                <label for="exampleInputEmail1">Email address</label>
-                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-                                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-                              </div>
-                              <div class="form-group col-4">
-                                <label for="exampleInputEmail1">Email address</label>
-                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-                                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-                              </div>
-                              <div class="form-group col-4">
-                                <label for="exampleInputEmail1">Email address</label>
-                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-                                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-                              </div>
-                            <div class="col-sm-12">
-                              <table class="table-detalle table table-bordered table-hover text-center" id="tableTransporte">
-                                <thead>
-                                  <tr>
-                                    <th>Razon Social</th>
-                                    <th>CUIT</th>
-                                    <th>Domicilio</th>
-                                  </tr>
-                                </thead>
-                                <tbody>
-                                  <tr id='addr0' data-id="0" style="display: none;">
-                                    <td data-name="razon_social">
-                                      <input name="razon_social" type="text" class="form-control" placeholder="Razon Social" name="razon_social" id="razon_social" required="required">
-                                    </td>
-                                    <td data-name="cuit">
-                                      <input name="cuit" type="text" class="form-control" placeholder="CUIT" name="cuit" id="cuit">
-                                    </td>
-                                    <td data-name="domicilio">
-                                      <input name="domicilio" type="text" class="form-control" placeholder="Domicilio" name="domicilio" id="domicilio">
-                                    </td>
-                                  </tr>
-                                </tbody>
-                              </table>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    
-                      <div class="row">
-                        <div class="col-sm-6">
-                          <div class="card-header">
-                              <h4 class="text-center">Choferes</h4>
-                          </div>
-                          <div class="form-group row">
-                            <div class="col-sm-12">
-                              <table class="table-detalle table table-bordered table-hover text-center" id="tableChofer">
-                                <thead>
-                                  <tr>
-                                    <th>Nombre</th>
-                                    <th>Apellido</th>
-                                    <th>Eliminar</th>
-                                  </tr>
-                                </thead>
-                                <tbody>
-                                  <tr id='addr0' data-id="0" style="display: none;">
-                                    <td data-name="nombre">
-                                      <input type="text" class="form-control" placeholder="Nombre" name="nombre[]" id="nombre-0"/>
-                                    </td>
-                                    <td data-name="apellido">
-                                      <input type="text" class="form-control" placeholder="Apellido" name="apellido[]" id="apellido-0"/>
-                                    </td>
-                                    <td data-name="eliminar">
-                                      <span name="eliminar[]" title="Eliminar" class="btn btn-sm row-remove text-center" onClick="eliminarFila(this);">
-                                        <img src="img/icon_baja.png" width="24" height="25" border="0" alt="Eliminar" title="Eliminar">
-                                      </span>
-                                    </td>
-                                  </tr>
-                                </tbody>
-                                <tfoot>
-                                  <tr>
-                                    <td colspan="3" align='right'>
-                                      <input type="button" class="btn btn-dark" id="addRowChofer" value="Agregar Chofer">
-                                      <input type="hidden" name="emailEliminados" id="emailEliminados">
-                                    </td>
-                                  </tr>
-                                </tfoot>
-                              </table>
-                            </div>
-                          </div>
-                        </div>
-
-                        <div class="col-sm-6">
-                          <div class="card-header">
-                              <h4 class="text-center">Vehiculos</h4>
-                          </div>
-                          <div class="card-body p-0">
                             <div class="form-group row">
-                              <div class="col-sm-6">
+                              <div class="form-group col-4">
+                                <label for="razon_social">Razon Social</label>
+                                <input type="text" class="form-control" id="razon_social" name="razon_social" aria-describedby="Razon Social" placeholder="Ingrese la Razon Social">
+                                <!-- <small id="razon_social" class="form-text text-muted">Razon Social</small> -->
+                              </div>
+
+                              <div class="form-group col-4">
+                                <label for="cuit">CUIT</label>
+                                <input type="text" class="form-control" id="cuit" name="cuit" aria-describedby="cuit" placeholder="Introduzca el CUIT">
+                                <!-- <small id="cuit" class="form-text text-muted">We'll never share your text with anyone else.</small> -->
+                              </div>
+                              
+                              <div class="form-group col-4">
+                                <label for="domicilio">Domicilio</label>
+                                <input type="text" class="form-control" id="domicilio" name="domicilio" aria-describedby="domicilio" placeholder="Introduzca el Domicilio">
+                                <!-- <small id="domicilio" class="form-text text-muted">We'll never share your text with anyone else.</small> -->
+                              </div>
+                            </div>
+                          </div><!-- .col -->
+                        </div><!-- .row -->
+                    
+                        <div class="row">
+                          <div class="col-sm-6">
+                            <div class="card-header">
+                                <h4 class="text-center">Choferes</h4>
+                            </div>
+                            <div class="card-body pt-0">
+                              <div class="form-group row">
+                                <table class="table-detalle table table-bordered table-hover text-center" id="tableChofer">
+                                  <thead>
+                                    <tr>
+                                      <th>Nombre y Apellido</th>
+                                      <th>DNI</th>
+                                      <th>Eliminar</th>
+                                    </tr>
+                                  </thead>
+                                  <tbody>
+                                    <tr id='addr0' data-id="0" style="display: none;">
+                                      <td data-name="nombre_apellido">
+                                        <input type="text" class="form-control" placeholder="Nombre y Apellido" name="nombre_apellido[]" id="nombre-0"/>
+                                      </td>
+                                      <td data-name="DNI">
+                                        <input type="text" class="form-control" placeholder="DNI" name="dni[]" id="dni-0"/>
+                                      </td>
+                                      <td data-name="eliminar">
+                                        <span name="eliminar[]" title="Eliminar" class="btn btn-sm row-remove text-center" onClick="eliminarFila(this);">
+                                          <img src="img/icon_baja.png" width="24" height="25" border="0" alt="Eliminar" title="Eliminar">
+                                        </span>
+                                      </td>
+                                    </tr>
+                                  </tbody>
+                                  <tfoot>
+                                    <tr>
+                                      <td colspan="3" align='right'>
+                                        <input type="button" class="btn btn-dark" id="addRowChofer" value="Agregar Chofer">
+                                      </td>
+                                    </tr>
+                                  </tfoot>
+                                </table>
+                              </div><!--.form-group-->
+                            </div><!--.card-body-->
+                          </div><!--.col-sm-6-->
+
+                          <div class="col-sm-6">
+                            <div class="card-header">
+                                <h4 class="text-center">Vehiculos</h4>
+                            </div>
+                            <div class="card-body pt-0">
+                              <div class="form-group row">
                                 <table class="table-detalle table table-bordered table-hover text-center" id="tableVehiculo">
                                   <thead>
                                     <tr>
@@ -191,42 +190,42 @@
                                       <td data-name="patente2">
                                         <input type="text" class="form-control" placeholder="Patente" name="patente2[]" id="patente2-0"/>
                                       </td>
-              
+                
                                       <td data-name="eliminar">
                                         <span name="eliminar[]" title="Eliminar" class="btn btn-sm row-remove text-center" onClick="eliminarFila(this);">
                                           <img src="img/icon_baja.png" width="24" height="25" border="0" alt="Eliminar" title="Eliminar">
                                         </span>
                                       </td>
                                     </tr>
-                                </tbody>
-                                <tfoot>
-                                  <tr>
-                                    <td colspan="4" align='right'>
-                                      <input type="button" class="btn btn-dark " id="addRowVehiculo" value="Agregar Vehiculo">
-                                      <input type="hidden" name="emailEliminados" id="emailEliminados">
-                                    </td>
-                                  </tr>
-                                </tfoot>
-                              </table>
-                            </div>  
-                          </div>
+                                  </tbody>
+                                  <tfoot>
+                                    <tr>
+                                      <td colspan="4" align='right'>
+                                        <input type="button" class="btn btn-dark " id="addRowVehiculo" value="Agregar Vehiculo">
+                                      </td>
+                                    </tr>
+                                  </tfoot>
+                                </table>
+                              </div>  
+                            </div><!--.card-body-->
+                          </div><!--col-sm-6-->
+                        </div>
+                      </div><!-- .card-body -->
+                      <div class="card-footer">
+                        <div class="col-sm-12 offset-sm-4">
+                          <button class="btn btn-primary" type="submit">Crear</button>
+						              <a href="listarTransportes.php" class="btn btn-light">Volver</a>
                         </div>
                       </div>
-                    <div class="card-footer">
-                      <div class="col-sm-12 offset-sm-9">
-                        <button class="btn btn-primary" type="submit">Crear</button>
-						            <a href="listarTransportes.php" class="btn btn-light">Volver</a>
-                      </div>
-                    </div>
-                  </form>
+                    </form><!--form -->
+                  </div>
                 </div>
               </div>
-            </div>
-          </div>
-          <!-- Container-fluid Ends-->
-        </div>
+            </div><!--.row-->
+          </div><!-- Container-fluid Ends-->
+        </div><!--.page-body-->
         <!-- footer start-->
-		<?php include("footer.php"); ?>
+		    <?php include("footer.php"); ?>
       </div>
     </div>
     <!-- latest jquery-->
