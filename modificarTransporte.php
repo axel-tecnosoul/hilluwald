be50d70ff9a18030059f72bd33053793ce15ba86
tombazan <tomasbazan95@gmail.com>
2024-02-29 00:33:03 -0300

    subiendo cambios a modificarTransporte PERO aun falta TERMINAR

--- modificarTransporte.php
+++ modificarTransporte.php
@@ -35,10 +35,20 @@
 		
 		$pdo = Database::connect();
 		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		$sql = "SELECT id, nombre, tipo, patente, id_usuario FROM transportes WHERE id = ? ";
+		$sql = "SELECT id, razon_social, cuit, domicilio, id_usuario FROM transportes WHERE id = ? ";
 		$q = $pdo->prepare($sql);
 		$q->execute(array($id));
 		$data = $q->fetch(PDO::FETCH_ASSOC);
+
+    $sql2 = "SELECT id, nombre_apellido, dni, id_transporte, id_usuario FROM choferes WHERE id_transporte = ? ";
+		$q2 = $pdo->prepare($sql2);
+		$q->execute(array($id));
+		$data2 = $q2->fetch(PDO::FETCH_ASSOC);
+
+    $sql3 = "SELECT id, descripcion, patente, patente2, id_transporte,id_usuario FROM vehiculos WHERE id_transporte = ? ";
+		$q3 = $pdo->prepare($sql3);
+		$q3->execute(array($id));
+		$data3 = $q3->fetch(PDO::FETCH_ASSOC);
 		
 		Database::disconnect();
 	}
@@ -94,30 +104,125 @@
                   <div class="card-header">
                     <h5>Modificar Transporte</h5>
                   </div>
-				  <form class="form theme-form" role="form" method="post" action="modificarTransporte.php?id=<?php echo $id?>">
+				          <form class="form theme-form" role="form" method="post" action="modificarTransporte.php?id=<?php echo $id?>">
                     <div class="card-body">
                       <div class="row">
                         <div class="col">
-							<div class="form-group row">
-								<label class="col-sm-3 col-form-label">Nombre</label>
-								<div class="col-sm-9"><input name="nombre" type="text" maxlength="99" class="form-control" value="<?php echo $data['nombre']; ?>" required="required"></div>
-							</div>
+                          <div class="form-group row">
+                            <div class="form-group col-4">
+                              <label for="razon_social">Razon Social</label>
+                              <input type="text" class="form-control" id="razon_social" name="razon_social" aria-describedby="Razon Social" placeholder="Ingrese la Razon Social" value="<?= $data['razon_social']; ?>">
+                              <!-- <small id="razon_social" class="form-text text-muted">Razon Social</small> -->
+                            </div>
+
+                            <div class="form-group col-4">
+                              <label for="cuit">CUIT</label>
+                              <input type="text" class="form-control" id="cuit" name="cuit" aria-describedby="cuit" placeholder="Introduzca el CUIT" value="<?= $data['cuit']; ?>">
+                              <!-- <small id="cuit" class="form-text text-muted">We'll never share your text with anyone else.</small> -->
+                            </div>
+                              
+                            <div class="form-group col-4">
+                              <label for="domicilio">Domicilio</label>
+                              <input type="text" class="form-control" id="domicilio" name="domicilio" aria-describedby="domicilio" placeholder="Introduzca el Domicilio" value="<?= $data['domicilio']; ?>">
+                              <!-- <small id="domicilio" class="form-text text-muted">We'll never share your text with anyone else.</small> -->
+                            </div>
+                          </div>
+                        </div><!-- .col -->
+                      </div><!-- .row -->
+                    
+                      <div class="row">
+                        <div class="col-sm-6">
+                          <div class="card-header">
+                              <h4 class="text-center">Choferes</h4>
+                          </div>
+                          <div class="card-body pt-0">
                             <div class="form-group row">
-								<label class="col-sm-3 col-form-label">Tipo</label>
-								<div class="col-sm-9"><input name="tipo" type="text" maxlength="99" class="form-control" value="<?php echo $data['tipo']; ?>" required="required"></div>
-							</div>
+                              <table class="table-detalle table table-bordered table-hover text-center" id="tableChofer">
+                                <thead>
+                                  <tr>
+                                    <th>Nombre y Apellido</th>
+                                    <th>DNI</th>
+                                    <th>Eliminar</th>
+                                  </tr>
+                                </thead>
+                                <tbody>
+                                  <tr id='addr0' data-id="0" style="display: none;">
+                                    <td data-name="nombre_apellido">
+                                      <input type="text" class="form-control" placeholder="Nombre y Apellido" name="nombre_apellido[]" id="nombre-0" value="<?= $data2['nombre_apellido']; ?>"/>
+                                    </td>
+                                    <td data-name="DNI">
+                                      <input type="text" class="form-control" placeholder="DNI" name="dni[]" id="dni-0" value="<?= $data2['dni']; ?>"/>
+                                    </td>
+                                    <td data-name="eliminar">
+                                      <span name="eliminar[]" title="Eliminar" class="btn btn-sm row-remove text-center" onClick="eliminarFila(this);">
+                                        <img src="img/icon_baja.png" width="24" height="25" border="0" alt="Eliminar" title="Eliminar">
+                                      </span>
+                                    </td>
+                                  </tr>
+                                </tbody>
+                                <tfoot>
+                                  <tr>
+                                    <td colspan="3" align='right'>
+                                      <input type="button" class="btn btn-dark" id="addRowChofer" value="Agregar Chofer">
+                                    </td>
+                                  </tr>
+                                </tfoot>
+                              </table>
+                            </div><!--.form-group-->
+                          </div><!--.card-body-->
+                        </div><!--.col-sm-6-->
+
+                        <div class="col-sm-6">
+                          <div class="card-header">
+                              <h4 class="text-center">Vehiculos</h4>
+                          </div>
+                          <div class="card-body pt-0">
                             <div class="form-group row">
-								<label class="col-sm-3 col-form-label">Patente</label>
-								<div class="col-sm-9"><input name="patente" type="text" maxlength="99" class="form-control" value="<?php echo $data['patente']; ?>" required="required"></div>
-							</div>
-                            
-                        </div>
+                              <table class="table-detalle table table-bordered table-hover text-center" id="tableVehiculo">
+                                <thead>
+                                  <tr>
+                                    <th>Descripcion</th>
+                                    <th>Patente</th>
+                                    <th>Patente2</th>
+                                    <th>Eliminar</th>
+                                  </tr>
+                                </thead>
+                                <tbody>
+                                  <tr id='addr0' data-id="0" style="display: none;">
+                                    <td data-name="descripcion">
+                                      <input type="text" class="form-control" placeholder="Descripcion" name="descripcion[]" id="descripcion-0" value="<?= $data3['descripcion']; ?>"/>
+                                    </td>
+                                    <td data-name="patente">
+                                      <input type="text" class="form-control" placeholder="Patente" name="patente[]" id="patente-0" value="<?= $data3['patente']; ?>"/>
+                                    </td>
+                                    <td data-name="patente2">
+                                      <input type="text" class="form-control" placeholder="Patente" name="patente2[]" id="patente2-0" value="<?= $data3['patente2']; ?>"/>
+                                    </td>
+                
+                                    <td data-name="eliminar">
+                                      <span name="eliminar[]" title="Eliminar" class="btn btn-sm row-remove text-center" onClick="eliminarFila(this);">
+                                        <img src="img/icon_baja.png" width="24" height="25" border="0" alt="Eliminar" title="Eliminar">
+                                      </span>
+                                    </td>
+                                  </tr>
+                                </tbody>
+                                <tfoot>
+                                  <tr>
+                                    <td colspan="4" align='right'>
+                                      <input type="button" class="btn btn-dark " id="addRowVehiculo" value="Agregar Vehiculo">
+                                    </td>
+                                  </tr>
+                                </tfoot>
+                              </table>
+                            </div>  
+                          </div><!--.card-body-->
+                        </div><!--col-sm-6-->
                       </div>
-                    </div>
+                    </div><!-- .card-body -->
                     <div class="card-footer">
-                      <div class="col-sm-9 offset-sm-3">
-                        <button class="btn btn-primary" type="submit">Modificar</button>
-						<a onclick="document.location.href='listarTransportes.php'" class="btn btn-light">Volver</a>
+                      <div class="col-sm-12 offset-sm-4">
+                        <button class="btn btn-primary" type="submit">Crear</button>
+						            <a href="listarTransportes.php" class="btn btn-light">Volver</a>
                       </div>
                     </div>
                   </form>
@@ -156,5 +261,161 @@
     <!-- Plugin used-->
 	<script src="assets/js/select2/select2.full.min.js"></script>
     <script src="assets/js/select2/select2-custom.js"></script>
+    <script type="text/javascript">
+
+      $(document).ready(function(){
+          $("#addRowChofer").on('click', function(event) {
+            event.preventDefault();
+            addRowChofer();
+          }).click();
+
+          $("#addRowVehiculo").on('click', function(event) {
+            event.preventDefault();
+            addRowVehiculo();
+          }).click();
+      });
+
+      function eliminarFila(t){
+        var fila=$(t).closest("tr");
+        fila.remove();
+      }
+
+      function addRowChofer(){
+        //alert("hola");
+        var newid = 0;
+        var primero="";
+        var ultimoRegistro=0;
+        $.each($("#tableChofer tr"), function() {
+          if (parseInt($(this).data("id")) > newid) {
+            newid = parseInt($(this).data("id"));
+          }
+        });
+        //debugger;
+        newid++;
+        //console.log(newid);
+        var tr = $("<tr></tr>", {
+          "id": "addr"+newid,
+          "data-id": newid
+        });
+        //console.log(newid);
+        var p=0;
+        $.each($("#tableChofer tbody tr:nth(0) td"),function(){//loop through each td and create new elements with name of newid
+          var cur_td = $(this); 
+          var children = cur_td.children();
+          if($(this).data("name")!=undefined){// add new td and element if it has a name
+            var td = $("<td></td>", {
+              "data-name": $(cur_td).data("name"),
+              "class": this.className
+            });
+            var c = $(cur_td).find($(children[0]).prop('tagName')).clone();//.val("")
+            
+            var id=$(c).attr("id");
+            $(c).attr("required",true);
+            ultimoRegistro=id;
+            if(id!=undefined){
+              //console.log("id1: ");
+              //console.log(id);
+              id=id.split("-");
+              c.attr("id", id[0]+"-"+newid);//modificamos el id de cada input
+              if(p==0){
+                primero=c;
+                p++;
+              }
+            }
+            c.appendTo($(td));
+            td.appendTo($(tr));
+            
+          }else {
+            //console.log("<td></td>",{'text':$('#tab_logic tr').length})
+            var td = $("<td></td>", {
+              'text': $('#tableChofer tr').length
+            }).appendTo($(tr));
+          }
+        });
+        //console.log($(tr).find($("input[name='detalledni[]']")));
+        //console.log(tr);//.find($("input"))
+        $(tr).appendTo($('#tableChofer'));// add the new row
+        if(newid>0){
+          primero.focus();
+          var sel2=$("#id_categoria-"+newid)
+          //console.log(sel2);
+          
+          sel2.select2();//llamamos para inicializar select2
+          sel2.select2('destroy');//como no se iniciliaza bien lo destruimos para que elimine las clases que arrastra de la clonacion
+          sel2.select2();//volvemos a inicializar y ahora si se inicializa bien
+          
+        }
+        return tr.attr("id");
+      }
+
+      function addRowVehiculo(){
+        //alert("hola");
+        var newid = 0;
+        var primero="";
+        var ultimoRegistro=0;
+        $.each($("#tableVehiculo tr"), function() {
+          if (parseInt($(this).data("id")) > newid) {
+            newid = parseInt($(this).data("id"));
+          }
+        });
+        //debugger;
+        newid++;
+        //console.log(newid);
+        var tr = $("<tr></tr>", {
+          "id": "addr"+newid,
+          "data-id": newid
+        });
+        //console.log(newid);
+        var p=0;
+        $.each($("#tableVehiculo tbody tr:nth(0) td"),function(){//loop through each td and create new elements with name of newid
+          var cur_td = $(this); 
+          var children = cur_td.children();
+          if($(this).data("name")!=undefined){// add new td and element if it has a name
+            var td = $("<td></td>", {
+              "data-name": $(cur_td).data("name"),
+              "class": this.className
+            });
+            var c = $(cur_td).find($(children[0]).prop('tagName')).clone();//.val("")
+            
+            var id=$(c).attr("id");
+            $(c).attr("required",true);
+            ultimoRegistro=id;
+            if(id!=undefined){
+              //console.log("id1: ");
+              //console.log(id);
+              id=id.split("-");
+              c.attr("id", id[0]+"-"+newid);//modificamos el id de cada input
+              if(p==0){
+                primero=c;
+                p++;
+              }
+            }
+            c.appendTo($(td));
+            td.appendTo($(tr));
+            
+          }else {
+            //console.log("<td></td>",{'text':$('#tab_logic tr').length})
+            var td = $("<td></td>", {
+              'text': $('#tableVehiculo tr').length
+            }).appendTo($(tr));
+          }
+        });
+        //console.log($(tr).find($("input[name='detalledni[]']")));
+        //console.log(tr);//.find($("input"))
+        $(tr).appendTo($('#tableVehiculo'));// add the new row
+        if(newid>0){
+          primero.focus();
+          var sel2=$("#id_categoria-"+newid)
+          //console.log(sel2);
+          
+          sel2.select2();//llamamos para inicializar select2
+          sel2.select2('destroy');//como no se iniciliaza bien lo destruimos para que elimine las clases que arrastra de la clonacion
+          sel2.select2();//volvemos a inicializar y ahora si se inicializa bien
+          
+        }
+        return tr.attr("id");
+      }
+
+    </script>
   </body>
 </html>
